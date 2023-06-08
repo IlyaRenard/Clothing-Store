3e51ed8ce0401390ad85d94acca4c5041053ccab
IlyaRenard <ilya.gavrilik.1998@gmail.com>
2023-06-08 14:13:48 +0300

    create ClothItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 
 import ClothesItem from './components/ClothesItem';
+import { IClothes } from './types/Clothes';
 
 function App() {
+
+  const clothes:IClothes ={
+    id: 1,
+    name: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+    description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+    price: 109.95,
+    image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+    isAvailability: false
+  }
+
   return (
     <div className="bg-dark-gray">
-      <ClothesItem/>
+      <div className='flex flex-row flex-wrap content-between justify-center'>
+      <ClothesItem clothes={clothes}/>
+      <ClothesItem clothes={clothes}/>
+      
+      </div>
+      
     </div>
   );
 }
--- src/components/ClothesItem.tsx
+++ src/components/ClothesItem.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { FC } from "react";
+import { IClothes } from "../types/Clothes";
+import { Rating } from "@mui/material";
 
-const ClothesItem = () => {
-  return (
-    <div>ClothesItem</div>
-  )
+interface IClothesItemProps {
+  clothes: IClothes;
 }
 
-export default ClothesItem
+const ClothesItem: FC<IClothesItemProps> = ({ clothes }) => {
+  const [rating, setRating] = React.useState<number | null>(0);
+
+  return (
+    <div className="relative w-[250px] h-[200px] m-2">
+      <div>
+        <img src={clothes.image} alt={clothes.image} className="" />
+        <button className="absolute top-0 right-0 m-2">❤️</button>
+      </div>
+      <div>
+        <h2 className="">{clothes.name}</h2>
+        <p>Цена: {clothes.price}</p>
+        <p>Описание: {clothes.description}</p>
+        <button>В корзину</button>
+      </div>
+      <div>
+        <Rating
+          value={rating}
+          onChange={(event, newValue) => {
+            setRating(newValue);
+          }}
+          className="!text-mint-green"
+        />
+        <p>Отзывы (0)</p>
+      </div>
+    </div>
+  );
+};
+
+export default ClothesItem;
